49f8828f816cf0d16576f967b486d97e8d761221
Buotangbata <103356537+Buotangbata@users.noreply.github.com>
2024-04-20 23:03:30 +0800

    added server action files

---
+++ src/actions/create-comment.ts
@@ -0,0 +1,5 @@
+'use server'
+
+export async function createComment(){
+     //to-do: revalidate POST SHOW PAGE
+}
---
+++ src/actions/create-post.ts
@@ -0,0 +1,5 @@
+'use server'
+
+export async function createPost(){
+     //to-do: revalidate TOPIC SHOW PAGE
+}
