d5d9632212b12eefb0b3027a57039caa20939932
Buotangbata <103356537+Buotangbata@users.noreply.github.com>
2024-04-22 21:38:33 +0800

    Finished header with sign in/out buttons

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,13 +7,48 @@ import {
     Input,
     Button,
     Avatar,
-    divider
+    Popover,
+    PopoverTrigger,
+    PopoverContent,
+    popover
 } from '@nextui-org/react';
 
 import { auth } from '@/auth';
+import * as actions from '@/actions';
 
 export default async function Header(){
     const session = await auth();
+
+    let authContent: React.ReactNode;
+
+    if(session?.user){
+        authContent = 
+        <Popover placement='left'>
+            <PopoverTrigger>
+                <Avatar src={session.user.image || ''}/>    
+            </PopoverTrigger>
+            <PopoverContent>
+                <div className='p-4'>
+                    <form action={actions.signOut}>
+                        <Button type='submit'>Sign Out</Button>
+                    </form>
+                </div>
+            </PopoverContent>
+        </Popover>
+    }else{
+        authContent = <>
+            <NavbarItem>
+                <form action={actions.signIn}>
+                    <Button type='submit' color='secondary' variant='bordered'>Sign In</Button>                    
+                </form>
+            </NavbarItem>
+            <NavbarItem>
+                <form action={actions.signIn}>
+                    <Button type='submit' color='primary' variant='flat'>Sign Up</Button>
+                </form>
+            </NavbarItem>
+        </>
+    }
 
     return (
         <Navbar className="chadow mb-6"> 
@@ -26,11 +61,7 @@ export default async function Header(){
                 </NavbarItem>
             </NavbarContent>
             <NavbarContent justify='end'>
-                <NavbarItem>
-                    {
-                        session?.user ? <div>Signed In</div> : <div>Signed Out</div>
-                    }
-                </NavbarItem>
+                    {authContent}
             </NavbarContent>
         </Navbar>
     );
